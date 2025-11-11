067ca3407993dedf301be29aaaeff15f02e7e2f7
Willow240 <astrid1spilmail@gmail.com>
2025-11-11 13:58:36 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_Willow240 Sorøtest (
+  tt_um_Willow240_Sorøtest (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
